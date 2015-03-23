89f1339721747ab433eec6a01e44b2adc773e5ab
Dony Wahyu Isp <dony_cavalera_md@yahoo.com>
2015-03-23 12:37:53 +0700

    Update function

--- Session/Session.php
+++ Session/Session.php
@@ -4,6 +4,24 @@ namespace Kecik;
 class Session {
 
 	public function __construct() {
+		session_start();
+	}
 
+	public function set($name, $value) {
+		$_SESSION[$name] = $value;
+	}
+
+	public function get($name) {
+		return $_SESSION[$name]
+	}
+
+	private function encrypt($plantext) {
+		$chipertext = '';
+		return $chipertext;
+	}
+
+	private function decrypt($chipertext) {
+		$plantext = '';
+		return $plantext;
 	}
 }
