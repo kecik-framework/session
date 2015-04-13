7cabbf046374d9ed0bdead32345b191e189e9c80
Dony Wahyu Isp <dony_cavalera_md@yahoo.com>
2015-04-13 19:22:46 +0700

    Fix Bug

--- Session/Session.php
+++ Session/Session.php
@@ -70,9 +70,6 @@ class Session {
 
 			if (empty($_SESSION['eivk'.md5($app->url->baseUrl())]) || !ctype_print($_SESSION['eivk'.md5($app->url->baseUrl())]))
 				unset($_COOKIE['eivk'.md5($app->url->baseUrl())]);
-			
-			if (empty($this->get('eivk'.md5($app->url->baseUrl()))) || !ctype_print($this->get('eivk'.md5($app->url->baseUrl()))))
-				unset($_SESSION['eivk'.md5($app->url->baseUrl())]);
 
             if (empty($_SESSION['eivk'.md5($app->url->baseUrl())]) && empty($_COOKIE['eivk'.md5($app->url->baseUrl())])) {
 				$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
